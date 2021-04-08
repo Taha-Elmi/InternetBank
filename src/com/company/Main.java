098ba8417cf7bb3f7fe8374c029ec699977bb772
TahaElmi <elmi.taha@gmail.com>
2021-04-09 01:23:18 +0430

    feat: main updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException{
 
         BankingSystem bankingSystem = new BankingSystem();
         User admin = new User("Taha", "Elmi", "sysadmin", "1234", true);
-        LinkedList<String> ui = new LinkedList<String>();
         UI display = new UI();
-        while (!display.getStatus().equals("exit"));
-        //TimeUnit.SECONDS.sleep(1);
+        while (!display.getStatus().equals("exit"))
+            display.menuLoop(bankingSystem);
     }
 
 }
